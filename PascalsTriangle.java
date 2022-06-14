8cf9726ea2d01ed4dd81db49f6ab9b419c5651cd
Saurav <saurav.dutta72@yahoo.com>
2022-06-14 14:48:23 +0530

    continue

---
+++ PascalsTriangle.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> output = new ArrayList<>();
+        if(numRows == 1)
+        {
+            return new ArrayList<>(
+                Arrays.asList(
+                    Arrays.asList(1)
+                )
+            );
+        }
+
+        for(int i = 0; i < numRows; i++)  {
+            output.add(new ArrayList<Integer>());
+        }
+
+        output.get(0).add(1);
+
+        for(int i=1;i<numRows;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                if(j==0)
+                {
+                    output.get(i).add(1);
+                    continue;
+                }
+
+                if(j==i)
+                {
+                    output.get(i).add(1);
+                    continue;
+                }
+
+                output.get(i).add(
+                    output.get(i-1).get(j-1) + output.get(i-1).get(j)
+                );
+            }
+        }
+
+        return output;
+    }
+}
